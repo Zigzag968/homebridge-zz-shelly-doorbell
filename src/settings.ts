785b0f689cd1a8d5e4b6f74050b85fe3913ff413
Alexandre Guibert <alexandre@evaneos.com>
2025-02-08 23:57:26 +0100

    wip

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,3 @@
 export const PLUGIN_NAME = 'homebridge-zz-shelly-doorbell';
-export const PLATFORM_NAME = 'HomebridgeShellyDoorbellPlatform';
+export const PLATFORM_NAME = 'ShellyDoorbell';
 export const DEFAULT_PORT = 8081;
