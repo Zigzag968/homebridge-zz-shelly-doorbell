955d64ac00ed77226a17c44e8afa8343d50f0a23
Alexandre Guibert <alexandre@evaneos.com>
2025-02-09 19:50:58 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -366,7 +366,7 @@ class DummyCameraAccessory {
     // Créer l'instance du délégué FFmpeg en passant undefined pour le CameraController
     const ffmpegDelegate = new UnifiedFfmpegDelegate(
       this.platform.log,
-      "-i ./media/fakeStreetImage.jpg",
+      `-i ${fakeStreetImagePath}`,
       accessory.displayName,
       hap
     );
